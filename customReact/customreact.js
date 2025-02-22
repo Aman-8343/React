c047a30960a00b43119c039cf13a24388cf295df
Aman kumar <amankumar105541@gmail.com>
2025-02-22 10:12:43 +0530

    Add files via upload

---
+++ customReact/customreact.js
@@ -0,0 +1,21 @@
+function CustomRender(reactElement,Container){
+    const domElement=document.createElement(reactElement.type)
+    domElement.innerHTML=reactElement.children
+    domElement.setAttribute('href',reactElement.props.href)
+    domElement.setAttribute('target',reactElement.props.target)
+    
+    Container.appendChild(domElement)
+}
+
+const reactElement={
+    type:'a',
+    props:{
+        href:'https://google.com',
+        target:'_blank'
+    },
+    children:'click me to visit google'
+}
+
+const mainContainer=document.querySelector('#root')
+
+CustomRender(reactElement,mainContainer)
